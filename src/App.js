1a6252db5bb1694ec935c7f58288d2b89600bc74
lyc2760008 <44538498+lyc2760008@users.noreply.github.com>
2020-07-07 23:47:42 -0600

    make it better

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <Header data={this.state.resumeData.main}/> */}
+        <Header data={this.state.resumeData.main}/>
         <About data={this.state.resumeData.main}/>
         <Resume data={this.state.resumeData.resume}/>
         <Portfolio data={this.state.resumeData.portfolio}/>
