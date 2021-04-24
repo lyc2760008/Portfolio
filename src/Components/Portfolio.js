5d3bf6919bab4b9fcfb05e777aeffe25c09c699f
lyc2760008 <44538498+lyc2760008@users.noreply.github.com>
2021-04-23 20:15:55 -0600

    test2

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,101 +1,117 @@
 import React, { Component } from 'react';
 
 class Portfolio extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//         name: '',
-//         email: 'yli7591@mtroyal.ca',
-//         message: '',
-//         disabled: false,
-//         emailSent: null,
-//     }
-// }
+  //   constructor(props) {
+  //     super(props);
+  //     this.state = {
+  //         name: '',
+  //         email: 'yli7591@mtroyal.ca',
+  //         message: '',
+  //         disabled: false,
+  //         emailSent: null,
+  //     }
+  // }
   render() {
 
-    if(this.props.data){
-      var projects = this.props.data.projects.map(function(projects){
-        var projectImage = 'images/portfolio/'+projects.image;
-        return <div key={projects.title} className="columns portfolio-item">
-           <div className="item-wrap">
-            <a href={projects.url} target="_blank" rel="noopener noreferrer" title={projects.title}>
-               <img alt={projects.title} src={projectImage} />
-               <div className="overlay">
-                  <div className="portfolio-item-meta">
-                 <h5>{projects.title}</h5>
-                     <p>{projects.category}</p>
-                  </div>
-                </div>
-              <div className="link-icon"><i className="fa fa-link"></i></div>
-            </a>
-          </div>
-        </div>
-        
-      })
+    // if (this.props.data) {
+    //   var projects = this.props.data.projects.map(function (projects) {
+    //     var projectImage = 'images/portfolio/' + projects.image;
+    //     return <div key={projects.title} className="columns portfolio-item">
+    //       <div className="item-wrap">
+    //         <a href={projects.url} target="_blank" rel="noopener noreferrer" title={projects.title}>
+    //           <img alt={projects.title} src={projectImage} />
+    //           <div className="overlay">
+    //             <div className="portfolio-item-meta">
+    //               <h5>{projects.title}</h5>
+    //               <p>{projects.category}</p>
+    //             </div>
+    //           </div>
+    //           <div className="link-icon"><i className="fa fa-link"></i></div>
+    //         </a>
+    //       </div>
+    //     </div>
 
-      var workProjects = this.props.data.workProjects.map(function(projects){
-        var projectImage = 'images/portfolio/'+projects.image;
-        return <div key={projects.title} className="columns portfolio-item">
-           <div className="item-wrap">
-            <a href={projects.url} target="_blank" rel="noopener noreferrer" title={projects.title}>
-               <img alt={projects.title} src={projectImage} />
-               <div className="overlay">
-                  <div className="portfolio-item-meta">
-                 <h5>{projects.title}</h5>
-                     <p>{projects.category}</p>
-                  </div>
-                </div>
-              <div className="link-icon"><i className="fa fa-link"></i></div>
-            </a>
-          </div>
-        </div>
-        
-      })
+    //   })
 
-      var workProjects2 = this.props.data.workProjects2.map(function(projects){
-        var projectImage = 'images/portfolio/'+projects.image;
-        return <div key={projects.title} className="columns portfolio-item">
-           <div className="item-wrap">
-            <div title={projects.title}>
-               <img alt={projects.title} src={projectImage} />
-               <div className="overlay">
-                  <div className="portfolio-item-meta">
-                 <h5>{projects.title}</h5>
-                     <p>{projects.category}</p>
-                  </div>
-                </div>
-              <div className="link-icon"><i className="fa fa-link"></i></div>
-            </div>
-          </div>
-        </div>
-        
-      })
-    }
+    //   var workProjects = this.props.data.workProjects.map(function (projects) {
+    //     var projectImage = 'images/portfolio/' + projects.image;
+    //     return <div key={projects.title} className="columns portfolio-item">
+    //       <div className="item-wrap">
+    //         <a href={projects.url} target="_blank" rel="noopener noreferrer" title={projects.title}>
+    //           <img alt={projects.title} src={projectImage} />
+    //           <div className="overlay">
+    //             <div className="portfolio-item-meta">
+    //               <h5>{projects.title}</h5>
+    //               <p>{projects.category}</p>
+    //             </div>
+    //           </div>
+    //           <div className="link-icon"><i className="fa fa-link"></i></div>
+    //         </a>
+    //       </div>
+    //     </div>
+
+    //   })
+
+    //   var workProjects2 = this.props.data.workProjects2.map(function (projects) {
+    //     var projectImage = 'images/portfolio/' + projects.image;
+    //     return <div key={projects.title} className="columns portfolio-item">
+    //       <div className="item-wrap">
+    //         <div title={projects.title}>
+    //           <img alt={projects.title} src={projectImage} />
+    //           <div className="overlay">
+    //             <div className="portfolio-item-meta">
+    //               <h5>{projects.title}</h5>
+    //               <p>{projects.category}</p>
+    //             </div>
+    //           </div>
+    //           <div className="link-icon"><i className="fa fa-link"></i></div>
+    //         </div>
+    //       </div>
+    //     </div>
+
+    //   })
+    // }
 
     return (
       <section id="portfolio">
 
-      <div className="row">
+        <div className="row">
 
-         <div className="twelve columns collapsed">
+          <div className="twelve columns collapsed">
 
             <h1>Work Related Projects</h1>
 
             {/* <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-                {workProjects}
+              {workProjects}
             </div>
 
             <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-                {workProjects2}
+              {workProjects2}
             </div> */}
 
             <h1>Personal Projects</h1>
             <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-                {projects}
+              {this.props.data && this.props.data.projects.map(projects => {
+                return <div key={projects.title} className="columns portfolio-item">
+                  <div className="item-wrap">
+                    <a href={projects.url} target="_blank" rel="noopener noreferrer" title={projects.title}>
+                      <img alt={projects.title} src={'images/portfolio/' + projects.image} />
+                      <div className="overlay">
+                        <div className="portfolio-item-meta">
+                          <h5>{projects.title}</h5>
+                          <p>{projects.category}</p>
+                        </div>
+                      </div>
+                      <div className="link-icon"><i className="fa fa-link"></i></div>
+                    </a>
+                  </div>
+                </div>
+
+              })}
             </div>
           </div>
-      </div>
-   </section>
+        </div>
+      </section>
     );
   }
 }
